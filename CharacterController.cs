6465935b1386882dc6529e67c7245433616241d3
Sych Anastasiia <92505584+Semi606@users.noreply.github.com>
2023-11-08 20:45:26 +0200

    Add files via upload

---
+++ CharacterController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f; // Швидкість руху персонажа
+
+    void Update()
+    {
+        // Отримуємо вхідні дані від гравця
+        float horizontalInput = Input.GetAxis("Horizontal"); // Для руху вліво/вправо
+        float verticalInput = Input.GetAxis("Vertical");     // Для руху вгору/вниз
+
+        // Рух персонажа
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
