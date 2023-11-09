c22331ce2532d24623a3954534e8b1185143d8b3
Sych Anastasiia <92505584+Semi606@users.noreply.github.com>
2023-11-09 08:53:52 +0200

    Update levelload.cs

--- levelload.cs
+++ levelload.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransition : MonoBehaviour
+public class levelload : MonoBehaviour
 {
     public string sceneToLoad; // Назва сцени, яку потрібно завантажити
 
